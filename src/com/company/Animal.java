8e9dc4d378da42c281b236e37bb8e222d5d843e3
Yehor160587 <lawyer1605@mail.com>
2022-05-24 13:08:53 +0300

    Animals

---
+++ src/com/company/Animal.java
@@ -0,0 +1,53 @@
+/*package com.company;
+
+public abstract class Animal {
+
+    public void voice() {
+        System.out.println("Голос!");
+    }
+}
+class Dog extends Animal {
+    public void voice() {
+        System.out.println("Гав-гав!");
+    }
+}
+class Cat extends Animal {
+    public void voice() {
+        System.out.println("Мяу!");
+    }
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        dog.voice();
+        cat.voice();
+    }
+}*/
+
+package com.company;
+
+ abstract class Animal {
+    void voice() {
+        System.out.println("Голос");}
+ }
+
+     class Dog extends Animal {
+         @Override
+         void voice() {
+             super.voice();
+             System.out.println("Гав-гав!");
+         }
+     }
+     class Cat extends Animal {
+     @Override
+         void voice() {
+         super.voice();
+             System.out.println("Мяу!");
+         }
+
+     public static void main(String[] args) {
+         Dog myDog = new Dog();
+         myDog.voice();
+         Cat myCat = new Cat();
+         myCat.voice();
+     }
+ }
