b9eb5624c89bf96ab179457096e1f5acd2f8dbfb
Yehor160587 <lawyer1605@mail.com>
2022-05-27 02:21:55 +0300

    Привет

---
+++ src/com/company/Color.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Color {
+    private String name;
+
+    Color(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Color is " + name;
+    }
+}
+
+
